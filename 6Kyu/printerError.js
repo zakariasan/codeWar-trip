a85fc882719e855bb8f834888620bd7d8f45693c
zakariasan <elitziko@gmail.com>
2022-06-25 23:39:00 +0100

    Printer Errors

---
+++ 6Kyu/printerError.js
@@ -0,0 +1,15 @@
+//You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+//The string has a length greater or equal to one and contains only letters from ato z.
+
+
+function printerError(s) {
+    // your code
+    var count = 0;
+    for(var i = 0; i < s.length; i++) {
+      if (s[i] > "m") {
+        count++;
+      }
+    }
+    return count+"/"+s.length;
+}
