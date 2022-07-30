d8188322cfc16bf59275788570194b9a65e6b83e
zakariasan <elitziko@gmail.com>
2022-07-30 20:38:50 +0100

    Returning Strings

---
+++ 8Kyu/greet.js
@@ -0,0 +1,9 @@
+
+/*Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".*/
+
+
+
+function greet(name){
+  //your code here
+  return `Hello, ${name} how are you doing today?`
+}
