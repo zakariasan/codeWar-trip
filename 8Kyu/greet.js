b895185c5f87a3875a0bfe1dd12217b7eba80ee6
zakariasan <elitziko@gmail.com>
2022-08-03 01:49:09 +0100

    Welcome

--- 8Kyu/greet.js
+++ 8Kyu/greet.js
@@ -3,7 +3,31 @@
 
 
 
-function greet(name){
+function greet1(name){
   //your code here
   return `Hello, ${name} how are you doing today?`
 }
+
+function greet2(lang) {
+  return langs[lang]||langs['english'];
+}
+
+var langs = {
+'english': 'Welcome',
+'czech': 'Vitejte',
+'danish': 'Velkomst',
+'dutch': 'Welkom',
+'estonian': 'Tere tulemast',
+'finnish': 'Tervetuloa',
+'flemish': 'Welgekomen',
+'french': 'Bienvenue',
+'german': 'Willkommen',
+'irish': 'Failte',
+'italian': 'Benvenuto',
+'latvian': 'Gaidits',
+'lithuanian': 'Laukiamas',
+'polish': 'Witamy',
+'spanish': 'Bienvenido',
+'swedish': 'Valkommen',
+'welsh': 'Croeso'
+};
