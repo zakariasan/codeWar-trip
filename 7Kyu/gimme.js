8e0732f92de6fe5a00ab7175569b91904d271704
zakariasan <elitziko@gmail.com>
2022-08-05 23:34:54 +0100

    Find the middle element

---
+++ 7Kyu/gimme.js
@@ -0,0 +1,10 @@
+/*As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.*/
+
+/*The input to the function will be an array of three distinct numbers (Haskell: a tuple).*/
+
+/*For example:*/
+
+/*gimme([2, 3, 1]) => 0*/
+function gimme(a) {
+  return a.indexOf(a.concat().sort(function(a, b) { return a - b })[1])
+}
