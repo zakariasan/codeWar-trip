9b33ce1c61ed5677265b0bce6036c2c0134e9f1f
zakariasan <elitziko@gmail.com>
2022-08-14 23:16:22 +0100

    Cat years, Dog years

---
+++ 8Kyu/humanYearsCatYearsDogYears.js
@@ -0,0 +1,45 @@
+
+/*Kata Task*/
+/*I have a cat and a dog.*/
+
+/*I got them at the same time as kitten/puppy. That was humanYears years ago.*/
+
+/*Return their respective ages now as [humanYears,catYears,dogYears]*/
+
+/*NOTES:*/
+
+/*humanYears >= 1*/
+/*humanYears are whole numbers only*/
+/*Cat Years*/
+/*15 cat years for first year*/
+/*+9 cat years for second year*/
+/*+4 cat years for each year after that*/
+/*Dog Years*/
+/*15 dog years for first year*/
+/*+9 dog years for second year*/
+/*+5 dog years for each year after that*/
+
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+  // Your code here!
+  
+  let cat = 0
+  let dog = 0;
+  for(let i = 1; i<=humanYears; i++ ) {
+    if(i == 1){
+      cat+=15;
+      dog+=15
+    }else if( i == 2 ){
+      cat += 9;
+      dog+= 9
+    }else{
+      cat+=4;
+      dog+=5
+    }
+    }
+    
+    return[humanYears, cat,dog]
+  
+  return [0,0,0];
+}
+
