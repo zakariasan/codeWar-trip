a5bedc09e8e544a448c3986a97ba30712a251f44
zakariasan <elitziko@gmail.com>
2022-08-24 14:37:51 +0100

    No zeros for heros

---
+++ 8Kyu/noBoringZeros.js
@@ -0,0 +1,19 @@
+/*Numbers ending with zeros are boring.*/
+
+/*They might be fun in your world, but not here.*/
+
+/*Get rid of them. Only the ending ones.*/
+
+/*1450 -> 145*/
+/*960000 -> 96*/
+/*1050 -> 105*/
+/*-1050 -> -105*/
+
+
+
+function noBoringZeros(n) {
+  if(n == 0) return 0;
+  while(n%10 == 0)
+    n=n/10;
+  return n
+}
