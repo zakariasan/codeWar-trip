45afa061eb7368a64acabfa79c6cb06d563526cd
zakariasan <elitziko@gmail.com>
2022-09-05 22:56:50 +0100

    Switch it Up

---
+++ 8Kyu/switchItUp.js
@@ -0,0 +1,14 @@
+//When provided with a number between 0-9, return it in words.
+
+//Input :: 1
+
+//Output :: "One".
+
+//If your language supports it, try using a
+
+
+function switchItUp(number){
+//Write your own Code!
+  let arr = ["Zero", "One", "Two","Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+return arr[number]
+  }
