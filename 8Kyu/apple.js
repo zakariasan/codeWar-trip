d82d9465543498a41f0f2f19848e6a841528e624
zakariasan <elitziko@gmail.com>
2022-11-17 23:47:20 +0100

    update apple js

--- 8Kyu/apple.js
+++ 8Kyu/apple.js
@@ -1,7 +1,6 @@
 //Task
 //Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
 
-//v
 function apple(x){
 	if(x*x > 1000)
 		return "It's hotter than the sun!!"
