785f74fa1a80effbceb06e78d56abb852e1524b9
zakariasan <elitziko@gmail.com>
2022-12-16 07:52:14 +0100

    Sort the Gift Code

---
+++ 7Kyu/sortGiftCode.js
@@ -0,0 +1,9 @@
+  function sortGiftCode(code){
+  //TODO 
+  return code.split('').sort((a,b)=> a.localeCompare(b)).join('')
+}
+
+
+
+
+/*Santa's senior gift organizer Elf developed a way to represent up to 26 gifts by assigning a unique alphabetical character to each gift. After each gift was assigned a character, the gift organizer Elf then joined the characters to form the gift ordering code.*/
