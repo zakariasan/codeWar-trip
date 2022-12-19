384f2a30b4e9bf40a40105e2ec3e487c06170cc2
zakariasan <elitziko@gmail.com>
2022-12-19 13:45:58 +0100

    Is there a vowel in there

---
+++ 8Kyu/isVow.js
@@ -0,0 +1,15 @@
+function isVow(a){
+  for (var i=0, l=a.length; i<l; ++i)
+  {
+    var char = String.fromCharCode(a[i])
+    if ('aeiou'.indexOf(char) !== -1)
+    a[i] = char;
+  }
+  
+  return a;
+}
+/*Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).*/
+
+/*If they are, change the array value to a string of that vowel.*/
+
+/*Return the resulting array.*/
