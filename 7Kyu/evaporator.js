817cda1f67cb2d1c99ea9e28fbea778fcd93cb29
zakariasan <elitziko@gmail.com>
2022-12-25 21:09:34 +0100

    Deodorant Evaporator

---
+++ 7Kyu/evaporator.js
@@ -0,0 +1,22 @@
+function evaporator(content, evap_per_day, threshold){ 
+  
+ 
+  let  days = 0
+    let curr = content 
+    
+    while (curr >= content * threshold/100.0)
+    { 
+        curr = curr * (1 - evap_per_day/100.0)
+        days += 1
+    }
+    return days
+
+}
+/*This program tests the life of an evaporator containing a gas.*/
+
+/*We know the content of the evaporator (content in ml), the percentage of foam or gas lost every day (evap_per_day) and the threshold (threshold) in percentage beyond which the evaporator is no longer useful. All numbers are strictly positive.*/
+
+/*The program reports the nth day (as an integer) on which the evaporator will be out of use.*/
+
+/*Example:*/
+/*evaporator(10, 10, 5) -> 29*/
