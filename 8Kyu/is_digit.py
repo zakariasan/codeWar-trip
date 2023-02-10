95c319a64f59622749c9eccf04112db063708c53
zakariasan <elitziko@gmail.com>
2023-02-10 12:36:18 +0100

    Regexp Basics - is it a digit?

---
+++ 8Kyu/is_digit.py
@@ -0,0 +1,8 @@
+def is_digit(n):
+    if(n<= '9' and n>='0'):  
+        return True 
+    return False
+    #your code here
+
+
+# Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
