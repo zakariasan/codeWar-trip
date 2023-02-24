eeb03a87dd485d8690155024ce93e335a6fa9a93
zakariasan <elitziko@gmail.com>
2023-02-24 14:57:50 +0100

    Largest pair sum in array

---
+++ 7Kyu/largest_pair_sum.py
@@ -0,0 +1,15 @@
+def largest_pair_sum(numbers):
+    nb1 = max(numbers)
+    nb2 = max(filter(lambda x: x!= nb1, numbers))
+    return nb1 + nb2
+
+
+# Given a sequence of numbers, find the largest pair sum in the sequence.
+
+# For example
+
+# [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+# [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+# Input sequence contains minimum two elements and every element is an integer.
+
+
