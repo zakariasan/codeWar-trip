c25cb8c156a94523bc95049a1cadc419aa657e4b
zakariasan <elitziko@gmail.com>
2023-02-24 15:11:50 +0100

    Largest pair sum in array erro handling

--- 7Kyu/largest_pair_sum.py
+++ 7Kyu/largest_pair_sum.py
@@ -1,9 +1,9 @@
 def largest_pair_sum(numbers):
     nb1 = max(numbers)
-    nb2 = max(filter(lambda x: x!= nb1, numbers))
-    return nb1 + nb2
-
-
+    tab2 = list(filter(lambda x: x!= nb1, numbers))
+    if(len(tab2) == len(numbers) - 1):
+        return nb1 + max(tab2)
+    return nb1 + nb1
 # Given a sequence of numbers, find the largest pair sum in the sequence.
 
 # For example
