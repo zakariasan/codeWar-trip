9dab567e3718bce4d94a1cd62d0135a3e44d0e0b
zakariasan <elitziko@gmail.com>
2023-03-08 18:34:09 +0100

    Switcheroo

---
+++ 7Kyu/switcheroo.py
@@ -0,0 +1,10 @@
+
+def switcheroo(string):
+    return ((string.replace('a','x')).replace('b','a')).replace('x','b')
+
+# Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+# Example:
+
+# 'acb' --> 'bca'
+# 'aabacbaa' --> 'bbabcabb'
