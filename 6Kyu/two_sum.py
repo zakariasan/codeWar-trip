e303911b11b915accddcdd41709ddf499529805e
zakariasan <elitziko@gmail.com>
2023-03-11 00:11:27 +0100

    Two Sum

--- 6Kyu/two_sum.py
+++ 6Kyu/two_sum.py
@@ -1,9 +1,14 @@
 def two_sum(numbers, target):
-    print(numbers, target)
-    list  = numbers
-    for nbr in numbers:
-        for nb in list:
-            if( numbers.index(nbr)!= numbers.index(nb)):
-                if(nb + nbr == target ):
-                    return [numbers.index(nbr), numbers.index(nb)]
-            
+    for i, nbr1 in enumerate(numbers):
+        for j, nb in enumerate(numbers):
+            if nbr1 + nb == target and i != j:
+                return (i, j)
+    return False
+
+
+
+# Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+# For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+# The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
