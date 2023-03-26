49516830e1424e7857db388b3a7fef0edc59bf86
zakariasan <elitziko@gmail.com>
2023-03-26 14:13:22 +0000

    Sorted? yes? no? how?

---
+++ 7Kyu/is_sorted_and_how.py
@@ -0,0 +1,16 @@
+def is_sorted_and_how(arr):
+    if arr == sorted(arr):
+        return 'yes, ascending' 
+    elif arr == sorted(arr)[::-1]:
+        return 'yes, descending'
+    else:
+        return 'no'
+
+# Complete the method which accepts an array of integers, and returns one of the following:
+
+# "yes, ascending" - if the numbers in the array are sorted in an ascending order
+# "yes, descending" - if the numbers in the array are sorted in a descending order
+# "no" - otherwise
+# You can assume the array will always be valid, and there will always be one correct answer.
+
+
