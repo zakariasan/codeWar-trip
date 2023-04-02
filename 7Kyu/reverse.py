fe510af3f9c07834e08fca7bd58a26e8a9a87f76
zakariasan <elitziko@gmail.com>
2023-04-02 06:12:50 +0000

    esreveR

---
+++ 7Kyu/reverse.py
@@ -0,0 +1,10 @@
+def reverse(lst):
+    em = list()
+    for item in lst:
+        em.insert(0,item)
+    return em
+
+
+# Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+# (the dedicated builtin(s) functionalities are deactivated)
